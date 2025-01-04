ad9dccf9eb65e3b8254b0d5f16b1d283c86d34e5
moulidos <moulidos15@gmail.com>
2025-01-04 08:35:44 +0530

    Add files via upload

---
+++ physics.py
@@ -0,0 +1,14 @@
+
+x=int(input("enter a x value: "))
+n=5*0.01155
+print("upper :",n)
+x1=x*0.01
+x2=x1**2
+l=(0.01155+x2)
+print("a**2+x**2 =",l)
+l1=l**1.5
+print("(a**2+x**2)**3/2 =",l1)
+a=2*l1
+print(n,"/",a)
+a1=n/a
+print(a1)
